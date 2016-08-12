05483e45aab95291fe0b0d626241bf1d45a9fe56
imadbenabdeslam <imadbenabdeslam19@gmail.com>
2016-08-12 20:19:00 +0200

    Remove unused import + commit to be sure it's updated

--- Imad/Empire/Empire.Model/TestConsoleApplication/Program.cs
+++ Imad/Empire/Empire.Model/TestConsoleApplication/Program.cs
@@ -1,10 +1,5 @@
 ﻿using Empire.Business.QueryHandler;
 using Empire.Model.DTOs.Product;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestConsoleApplication
 {
